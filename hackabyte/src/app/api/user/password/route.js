d400354218e34f5aca528f63c77a430153032dad
Decaaaa <Avkb8237@gmail.com>
2025-03-15 18:11:29 -0700

    Reapply "WIP"
    This reverts commit 56b7d7a00ad33ff5839cf47b61381e28babd6c44.

--- hackabyte/src/app/api/user/password/route.js
+++ hackabyte/src/app/api/user/password/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { hash, compare } from 'bcrypt';
+import { hash, compare } from 'bcryptjs';
 import dbConnect from '@/lib/mongodb';
 import User from '@/models/User';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
